48d030e718c112ca0c56a053b9d5f69fe24c3cd3
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-12-14 17:53:30 -0500

    got some artists working

--- index2.js
+++ index2.js
@@ -1,7 +1,6 @@
 
-
 $(document).ready(function(){
-  var artists = ["Gary Clark Jr", "John Fahey", "John Lee Hooker", "Lightnin' Hopkins", "Muddy Waters"]
+  var artists = ["John Lee Hooker", "Lightnin' Hopkins", "Mississippi John Hurt", "Charlie Patton", "Elizabeth Cotten", "Jerry Garcia", "Mance Lipscomb", "Sonny Boy Williamson"]
   var options = artists.map(function(artist){
     return `<option value=${artist.split(' ').join('+')}>${artist}</option>`
   })
@@ -10,12 +9,13 @@ $(document).ready(function(){
     $('.dropDown').append(options[z])
   }
 
-
   $('.dropDown').change(function(){
+    console.log('hi')
     var artist = $('.dropDown').val()
+    console.log(artist)
     var shows
     var identifiers
-    var url = `https://archive.org/advancedsearch.php?q=creator%3A%22Muddy+Waters%22&fl%5B%5D=format&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=200&page=1&output=json&callback=callback&save=yes#raw`
+    var url = `https://archive.org/advancedsearch.php?q=creator%3A%22${artist}%22&fl%5B%5D=creator&fl%5B%5D=format&fl%5B%5D=genre&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=name&fl%5B%5D=title&sort%5B%5D=date+asc&sort%5B%5D=&sort%5B%5D=&rows=50000&page=1&output=json&callback=callback&save=yes#raw`
     $.ajax({
       headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
@@ -25,10 +25,20 @@ $(document).ready(function(){
       dataType: 'jsonp',
       jsonpCallback: 'callback'
     }).then((response) => {
-    // }).then((response) => {
+      var mps = response.response.docs.filter(function(item){
+        return item.mediatype === 'audio'
+      })
+      console.log(mps)
       var mps = response.response.docs.filter(function(item){
         for (j = 0; j < item.format.length; j++){
-          if (item.format[j] === 'VBR MP3'){
+          if (item.format[j] === 'MP3 Sample') {
+            return
+          }
+          else if (item.format[j] === '128Kbps MP3'){
+            return item
+          } else if (item.format[j] === '64Kbps MP3'){
+            return item
+          } else if (item.format[j] === 'VBR MP3'){
             return item
           }
         }
@@ -39,66 +49,47 @@ $(document).ready(function(){
       })
       console.log(identifiers)
       var selectedId = identifiers[Math.floor(Math.random() * identifiers.length)]
-      var finalShows = []
-      var filteredAlbums
       console.log(selectedId)
-
-        var url2 = `https://archive.org/metadata/${selectedId}`
-        $.ajax({
-          headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-          url: url2,
-          type: 'get',
-          dataType: 'jsonp'
-        }).then((response) => {
-          console.log(response)
-
-          var songs = response.files
-
-
-          var d1 = response.d1
-          var dir = response.dir
-          var shows = response.files.filter(function(item){
-            if (item.format === 'VBR MP3' && item.creator) {
-              return item.creator.includes(artist.split('+')[0])
-            } else if (item.format === 'VBR MP3' && item.artist){
-              return item.artist.includes(artist.split('+')[0])
-            }
-          })
-      console.log('hi')
-      console.log(shows)
-      var albums = shows.map(function(song){
-        var songInfo = {
-          artist: song.creator,
-          album: song.album,
-          song: song.title,
-          url: `https://${d1}${dir}/${song.name}`
-        }
-        return songInfo
-      })
-      console.log(albums)
-      var filteredAlbums = []
-      var finalAlbums = []
-      for (b = 0; b < albums.length; b++) {
-        if (albums[b].album) {
-          if (albums[b].album.includes('Sampler') === false && albums[b].album.includes('sample') === false && albums[b].album.includes('Blue Yule') === false && albums[b].album.includes('Taste Of Tradition') === false) {
-            filteredAlbums.push(albums[b])
-            }
-          }
-        }
-      var filtered2 = filteredAlbums.filter(function(album){
-        if (album.length > 0){
-          return album
-        }
-      })
-
-      console.log(filtered2)
-
+      var url2 = `https://archive.org/metadata/${selectedId}`
+      $.ajax({
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: url2,
+        type: 'get',
+        dataType: 'jsonp',
+        jsonpCallback: 'callback'
+      }).then((response) => {
+        console.log(response)
+        var songs = response.files.filter(function(item){
+            return item.format === '128Kbps MP3' || item.format === '64Kbps' || item.format === 'VBR MP3'
         })
+        console.log(songs)
+        var d1 = response.d1
+        var dir = response.dir
+        var shows = response.files
+        var metadata = songs.map(function(song){
+          var songInfo = {
+            artist: song.creator,
+            album: song.album,
+            song: song.title,
+            url: `https://${d1}${dir}/${song.name}`
+          }
+          return songInfo
+        })
+        console.log(metadata)
+        var selectedSong = metadata[Math.floor(Math.random() * metadata.length)]
+        console.log(selectedSong)
+        $('audio').attr('src', selectedSong.url)
+      })
     })
-
   })
+
+
+  
+
+
+
 
 
 })
