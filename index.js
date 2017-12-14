18f9c0286ec9beaa692e7b044fd7ccfbb44cd5dd
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-12-14 12:05:31 -0500

    ajax working

--- index.js
+++ index.js
@@ -1,106 +1,16 @@
-
-
 $(document).ready(function(){
-  var artists = ["Gary Clark Jr", "John Fahey", "John Lee Hooker", "Lightnin' Hopkins", "Muddy Waters"]
-  var options = artists.map(function(artist){
-    return `<option value=${artist.split(' ').join('+')}>${artist}</option>`
-  })
-  $('.dropDown').append(`<option>Choose An Artist</option>`)
-  for (z = 0; z < options.length; z++) {
-    $('.dropDown').append(options[z])
-  }
-
-
-  $('.dropDown').change(function(){
-    var artist = $('.dropDown').val()
-    var shows
-    var identifiers
-    var url = `https://archive.org/advancedsearch.php?q=Muddy+Waters&fl%5B%5D=identifier&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=200&page=1&output=json&callback=callback&save=yes#raw`
-    $.ajax({
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-      url: url,
-      type: 'get',
-      dataType: 'jsonp',
-    }).then((response) => {
-    // }).then((response) => {
-      console.log(response)
-
-    })
-
-  })
+var url = `https://archive.org/advancedsearch.php?q=creator%3A+Muddy+Watters&fl%5B%5D=avg_rating&fl%5B%5D=backup_location&fl%5B%5D=btih&fl%5B%5D=call_number&fl%5B%5D=collection&fl%5B%5D=contributor&fl%5B%5D=coverage&fl%5B%5D=creator&fl%5B%5D=date&fl%5B%5D=description&fl%5B%5D=downloads&fl%5B%5D=external-identifier&fl%5B%5D=foldoutcount&fl%5B%5D=format&fl%5B%5D=headerImage&fl%5B%5D=identifier&fl%5B%5D=imagecount&fl%5B%5D=language&fl%5B%5D=licenseurl&fl%5B%5D=mediatype&fl%5B%5D=members&fl%5B%5D=month&fl%5B%5D=num_reviews&fl%5B%5D=oai_updatedate&fl%5B%5D=publicdate&fl%5B%5D=publisher&fl%5B%5D=related-external-id&fl%5B%5D=reviewdate&fl%5B%5D=rights&fl%5B%5D=scanningcentre&fl%5B%5D=source&fl%5B%5D=stripped_tags&fl%5B%5D=subject&fl%5B%5D=title&fl%5B%5D=type&fl%5B%5D=volume&fl%5B%5D=week&fl%5B%5D=year&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=1000&page=1&output=json&callback=callback&save=yes`
+$.ajax({
+  headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+  url: url,
+  type: 'get',
+  dataType: 'jsonp',
+}).then((response) => {
+// }).then((response) => {
+  console.log(response)
 
 })
-      // var identifiers1 = response.response.docs.filter(function(item){
-      //   return item.format === 'VBR MP3'
-      // })
-      // var identifiers = identifiers1.map(function(item){
-      //   return item.identifier
-      // })
-      // console.log(identifiers)
-      // var selectedId = identifiers[Math.floor(Math.random() * identifiers.length)]
-      // var finalShows = []
-      // var filteredAlbums
-      // // for (i = 0; i < identifiers.length; i++) {
-      //
-      //   var url2 = `https://archive.org/metadata/${selectedId}`
-      //   $.ajax({
-      //     headers: {
-      //         'Content-Type': 'application/x-www-form-urlencoded'
-      //       },
-      //     url: url2,
-      //     type: 'get',
-      //     dataType: 'jsonp'
-      //   }).then((response) => {
-      //     console.log(response)
 
-          // var songs = reponse.files
-          //
-          //
-          // var d1 = response.d1
-          // var dir = response.dir
-          // var shows = response.files.filter(function(item){
-          //   if (item.format === 'VBR MP3' && item.creator) {
-          //     return item.creator.includes(artist.split('+')[0])
-          //   } else if (item.format === 'VBR MP3' && item.artist){
-          //     return item.artist.includes(artist.split('+')[0])
-          //   }
-          // })
-      // console.log('hi')
-      // console.log(shows)
-      // var albums = shows.map(function(song){
-      //   var songInfo = {
-      //     artist: song.creator,
-      //     album: song.album,
-      //     song: song.title,
-      //     url: `https://${d1}${dir}/${song.name}`
-      //   }
-      //   return songInfo
-      // })
-      // console.log(albums)
-      // var filteredAlbums = []
-      // var finalAlbums = []
-      // for (b = 0; b < albums.length; b++) {
-      //   if (albums[b].album) {
-      //     if (albums[b].album.includes('Sampler') === false && albums[b].album.includes('sample') === false && albums[b].album.includes('Blue Yule') === false && albums[b].album.includes('Taste Of Tradition') === false) {
-      //       filteredAlbums.push(albums[b])
-      //       }
-      //     }
-      //   }
-      // var filtered2 = filteredAlbums.filter(function(album){
-      //   if (album.length > 0){
-      //     return album
-      //   }
-      // })
-      //
-      // console.log(filtered2)
-      //
-      //   })
-      // }
-//     })
-//
-//   })
-//
-//
-// })
+})
