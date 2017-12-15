81591c9caee81d92545dc3f2bdd095f480db6c78
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-12-14 22:36:30 -0500

    styles

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-  var artists = ["John Lee Hooker", "Lightnin' Hopkins", "Mississippi John Hurt", "Charlie Patton", "Elizabeth Cotten", "Jerry Garcia", "Mance Lipscomb", "Sonny Boy Williamson"]
+  var artists = ["John Lee Hooker", "Lightnin' Hopkins", "Keller Williams", "Yonder Mountain String Band", "My Morning Jacket", "Bill Monroe", "Sonny Osborne", "Jimmie Skinner", "Kurt Vile", "Robert Randolph", "Papa String Band", "Jake Shimabukuro", "Widespread Panic", "Grateful Dead", "Mississippi John Hurt", "KEXP", "Steep Canyon Rangers", "Acoustic Junction", "Jerry Garcia", "Mance Lipscomb", "Sonny Boy Williamson"]
   var options = artists.map(function(artist){
     return `<option value=${artist.split(' ').join('+')}>${artist}</option>`
   })
@@ -8,8 +8,27 @@ $(document).ready(function(){
   for (z = 0; z < options.length; z++) {
     $('.dropDown').append(options[z])
   }
+  // var genres = ["Bluegrass"]
+  // var options2 = genres.map(function(genre){
+  //   return `<option value=${genre.split(' ').join('+')}>${genre}</option>`
+  // })
+  // $('.genre').append(`<option>Choose A Genre</option>`)
+  // for (e = 0; e < options2.length; e++) {
+  //   $('.genre').append(options2[e])
+  // }
+
 
   $('.dropDown').change(function(){
+    $('.nowPlaying').empty()
+    getSong()
+  })
+
+  $('#next').click(function(){
+    $('.nowPlaying').empty()
+    getSong()
+  })
+
+  function getSong(){
     console.log('hi')
     var artist = $('.dropDown').val()
     console.log(artist)
@@ -62,7 +81,7 @@ $(document).ready(function(){
       }).then((response) => {
         console.log(response)
         var songs = response.files.filter(function(item){
-            return item.format === '128Kbps MP3' || item.format === '64Kbps' || item.format === 'VBR MP3'
+            return item.format === '128Kbps MP3' || item.format === '64Kbps MP3' || item.format === 'VBR MP3'
         })
         console.log(songs)
         var d1 = response.d1
@@ -81,15 +100,92 @@ $(document).ready(function(){
         var selectedSong = metadata[Math.floor(Math.random() * metadata.length)]
         console.log(selectedSong)
         $('audio').attr('src', selectedSong.url)
+        if (selectedSong.artist) {
+          $('.nowPlaying').append(`<h2>${selectedSong.artist}</h2>`)
+        }
+        if (selectedSong.song){
+          $('.nowPlaying').append(`<h2>${selectedSong.song}</h2>`)
+        }
+        if (selectedSong.album){
+          $('.nowPlaying').append(`<h2>${selectedSong.album}</h2>`)
+        }
       })
     })
-  })
-
-
-  
-
-
-
-
-
+  }
 })
+
+  // $('.genre').change(function(){
+  //   console.log('hi')
+  //   var genre = $('.genre').val()
+  //   console.log(genre)
+  //   var shows
+  //   var identifiers
+  //   var url = `https://archive.org/advancedsearch.php?q=genre%3A%22${genre}%22&fl%5B%5D=creator&fl%5B%5D=format&fl%5B%5D=genre&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=name&fl%5B%5D=title&sort%5B%5D=date+asc&sort%5B%5D=&sort%5B%5D=&rows=10000&page=1&output=json&callback=callback&save=yes#raw`
+  //   $.ajax({
+  //     headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded'
+  //       },
+  //     url: url,
+  //     type: 'get',
+  //     dataType: 'jsonp',
+  //     jsonpCallback: 'callback'
+  //   }).then((response) => {
+  //     var mps = response.response.docs.filter(function(item){
+  //       return item.mediatype === 'audio'
+  //     })
+  //     console.log(mps)
+  //     var mps = response.response.docs.filter(function(item){
+  //       for (j = 0; j < item.format.length; j++){
+  //         if (item.format[j] === 'MP3 Sample') {
+  //           return
+  //         }
+  //         else if (item.format[j] === '128Kbps MP3'){
+  //           return item
+  //         } else if (item.format[j] === '64Kbps MP3'){
+  //           return item
+  //         } else if (item.format[j] === 'VBR MP3'){
+  //           return item
+  //         }
+  //       }
+  //     })
+  //     console.log(mps)
+  //     var identifiers = mps.map(function(item){
+  //       return item.identifier
+  //     })
+  //     console.log(identifiers)
+  //     var selectedId = identifiers[Math.floor(Math.random() * identifiers.length)]
+  //     console.log(selectedId)
+  //     var url2 = `https://archive.org/metadata/${selectedId}`
+  //     $.ajax({
+  //       headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded'
+  //       },
+  //       url: url2,
+  //       type: 'get',
+  //       dataType: 'jsonp',
+  //       jsonpCallback: 'callback'
+  //     }).then((response) => {
+  //       console.log(response)
+  //       var songs = response.files.filter(function(item){
+  //           return item.format === '128Kbps MP3' || item.format === '64Kbps' || item.format === 'VBR MP3'
+  //       })
+  //       console.log(songs)
+  //       var d1 = response.d1
+  //       var dir = response.dir
+  //       var shows = response.files
+  //       var metadata = songs.map(function(song){
+  //         var songInfo = {
+  //           artist: song.creator,
+  //           album: song.album,
+  //           song: song.title,
+  //           url: `https://${d1}${dir}/${song.name}`
+  //         }
+  //         return songInfo
+  //       })
+  //       console.log(metadata)
+  //       var selectedSong = metadata[Math.floor(Math.random() * metadata.length)]
+  //       console.log(selectedSong)
+  //       $('audio').attr('src', selectedSong.url)
+  //     })
+  //   })
+  // })
