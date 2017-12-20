371d941baa64da1eeefdb80e4c7468467a9630fc
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-12-19 22:21:58 -0500

    festivals

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-  var artists = ["Keller Williams", "Yonder Mountain String Band", "Robert Randolph", "Grateful Dead", "David Grisman", "Bob Weir", "Justin Townes Earle"]
+  var artists = ["Keller Williams", "Yonder Mountain String Band", "Robert Randolph", "Grateful Dead", "David Grisman", "Bob Weir", "Justin Townes Earle", "JJ Grey"]
   var options = artists.map(function(artist){
     return `<option value=${artist.split(' ').join('+')}>${artist}</option>`
   })
@@ -8,14 +8,14 @@ $(document).ready(function(){
   for (z = 0; z < options.length; z++) {
     $('.dropDown').append(options[z])
   }
-  // var genres = ["Bluegrass"]
-  // var options2 = genres.map(function(genre){
-  //   return `<option value=${genre.split(' ').join('+')}>${genre}</option>`
-  // })
-  // $('.genre').append(`<option>Choose A Genre</option>`)
-  // for (e = 0; e < options2.length; e++) {
-  //   $('.genre').append(options2[e])
-  // }
+  var genres = ["Bonnaroo", "Jazz Fest", "Outside Lands", "Newport Folk Festival", "Telluride Bluegrass Festival", "MerleFest", "Hardly Strictly Bluegrass", "Durango", "Juke Joint Festival", "Waterfront Blues Festival"]
+  var options2 = genres.map(function(genre){
+    return `<option value=${genre.split(' ').join('+')}>${genre}</option>`
+  })
+  $('.genre').append(`<option>Choose A Festival</option>`)
+  for (e = 0; e < options2.length; e++) {
+    $('.genre').append(options2[e])
+  }
 
 
   $('.dropDown').change(function(){
@@ -23,14 +23,27 @@ $(document).ready(function(){
     getSong()
   })
 
+  $('.genre').change(function(){
+    $('.nowPlaying').empty()
+    getFesty()
+  })
+
   $('#next').click(function(){
     $('.nowPlaying').empty()
-    getSong()
+      if ($('.dropDown').val() !== 'Choose An Artist'){
+        getSong()
+      } else {
+        getFesty()
+      }
   })
 
   $('audio').on('ended', function(){
     $('.nowPlaying').empty()
-    getSong()
+    if ($('.dropDown').val() !== 'Choose An Artist'){
+      getSong()
+    } else {
+      getFesty()
+    }
   })
 
   function getSong(){
@@ -123,78 +136,91 @@ $(document).ready(function(){
   }
 })
 
-  // $('.genre').change(function(){
-  //   console.log('hi')
-  //   var genre = $('.genre').val()
-  //   console.log(genre)
-  //   var shows
-  //   var identifiers
-  //   var url = `https://archive.org/advancedsearch.php?q=genre%3A%22${genre}%22&fl%5B%5D=creator&fl%5B%5D=format&fl%5B%5D=genre&fl%5B%5D=identifier&fl%5B%5D=mediatype&fl%5B%5D=name&fl%5B%5D=title&sort%5B%5D=date+asc&sort%5B%5D=&sort%5B%5D=&rows=10000&page=1&output=json&callback=callback&save=yes#raw`
-  //   $.ajax({
-  //     headers: {
-  //         'Content-Type': 'application/x-www-form-urlencoded'
-  //       },
-  //     url: url,
-  //     type: 'get',
-  //     dataType: 'jsonp',
-  //     jsonpCallback: 'callback'
-  //   }).then((response) => {
-  //     var mps = response.response.docs.filter(function(item){
-  //       return item.mediatype === 'audio'
-  //     })
-  //     console.log(mps)
-  //     var mps = response.response.docs.filter(function(item){
-  //       for (j = 0; j < item.format.length; j++){
-  //         if (item.format[j] === 'MP3 Sample') {
-  //           return
-  //         }
-  //         else if (item.format[j] === '128Kbps MP3'){
-  //           return item
-  //         } else if (item.format[j] === '64Kbps MP3'){
-  //           return item
-  //         } else if (item.format[j] === 'VBR MP3'){
-  //           return item
-  //         }
-  //       }
-  //     })
-  //     console.log(mps)
-  //     var identifiers = mps.map(function(item){
-  //       return item.identifier
-  //     })
-  //     console.log(identifiers)
-  //     var selectedId = identifiers[Math.floor(Math.random() * identifiers.length)]
-  //     console.log(selectedId)
-  //     var url2 = `https://archive.org/metadata/${selectedId}`
-  //     $.ajax({
-  //       headers: {
-  //         'Content-Type': 'application/x-www-form-urlencoded'
-  //       },
-  //       url: url2,
-  //       type: 'get',
-  //       dataType: 'jsonp',
-  //       jsonpCallback: 'callback'
-  //     }).then((response) => {
-  //       console.log(response)
-  //       var songs = response.files.filter(function(item){
-  //           return item.format === '128Kbps MP3' || item.format === '64Kbps' || item.format === 'VBR MP3'
-  //       })
-  //       console.log(songs)
-  //       var d1 = response.d1
-  //       var dir = response.dir
-  //       var shows = response.files
-  //       var metadata = songs.map(function(song){
-  //         var songInfo = {
-  //           artist: song.creator,
-  //           album: song.album,
-  //           song: song.title,
-  //           url: `https://${d1}${dir}/${song.name}`
-  //         }
-  //         return songInfo
-  //       })
-  //       console.log(metadata)
-  //       var selectedSong = metadata[Math.floor(Math.random() * metadata.length)]
-  //       console.log(selectedSong)
-  //       $('audio').attr('src', selectedSong.url)
-  //     })
-  //   })
-  // })
+  function getFesty(){
+    console.log('hi')
+    var genre = $('.genre').val()
+    console.log(genre)
+    var shows
+    var identifiers
+    var url = `https://archive.org/advancedsearch.php?q=title%3A%22${genre}%22&fl%5B%5D=creator&fl%5B%5D=external-identifier&fl%5B%5D=format&fl%5B%5D=identifier&fl%5B%5D=mediatype&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50000&page=1&output=json&callback=callback&save=yes#raw`
+    $.ajax({
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+      url: url,
+      type: 'get',
+      dataType: 'jsonp',
+      jsonpCallback: 'callback'
+    }).then((response) => {
+      var mps = response.response.docs.filter(function(item){
+        return item.mediatype === 'audio'
+      })
+      console.log(mps)
+      var mps = response.response.docs.filter(function(item){
+        for (j = 0; j < item.format.length; j++){
+          if (item.format[j] === 'MP3 Sample') {
+            return
+          }
+          else if (item.format[j] === '128Kbps MP3'){
+            return item
+          } else if (item.format[j] === '64Kbps MP3'){
+            return item
+          } else if (item.format[j] === 'VBR MP3'){
+            return item
+          }
+        }
+      })
+      console.log(mps)
+      var identifiers = mps.map(function(item){
+        return item.identifier
+      })
+      console.log(identifiers)
+      var selectedId = identifiers[Math.floor(Math.random() * identifiers.length)]
+      console.log(selectedId)
+      var url2 = `https://archive.org/metadata/${selectedId}`
+      $.ajax({
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        url: url2,
+        type: 'get',
+        dataType: 'jsonp',
+        jsonpCallback: 'callback'
+      }).then((response) => {
+        console.log(response)
+        var songs = response.files.filter(function(item){
+            return item.format === '128Kbps MP3' || item.format === '64Kbps' || item.format === 'VBR MP3'
+        })
+        console.log(songs)
+        var d1 = response.d1
+        var dir = response.dir
+        var shows = response.files
+        var metadata = songs.map(function(song){
+          var songInfo = {
+            artist: song.creator,
+            album: song.album,
+            song: song.title,
+            url: `https://${d1}${dir}/${song.name}`
+          }
+          return songInfo
+        })
+        console.log(metadata)
+        var selectedSong = metadata[Math.floor(Math.random() * metadata.length)]
+        console.log(selectedSong)
+        $('audio').attr('src', selectedSong.url)
+        $('.nowPlaying').append(`<h2>Now playing: ${$('.genre').val().split('+').join(' ')} radio</h2>`)
+        if (selectedSong.artist) {
+          $('.nowPlaying').append(`<h2>${selectedSong.artist}</h2>`)
+        }
+        if (selectedSong.song){
+          $('.nowPlaying').append(`<h2>${selectedSong.song}</h2>`)
+        }
+        if (selectedSong.album){
+          $('.nowPlaying').append(`<h2>${selectedSong.album}</h2>`)
+        }
+        if (!selectedSong.artist && !selectedSong.song && !selectedSong.album){
+          $('.nowPlaying').append(`<h2>NO SONG INFO AVAILABLE</h2>`)
+        }
+      })
+    })
+  }
